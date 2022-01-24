de209b117036120187b403e74d8da1557330f8aa
Jiaming Su <sjm.zax@gmail.com>
2022-01-24 07:56:43 -0500

    refactor(note): error handling

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -18,6 +18,7 @@ import java.util.List;
 public class HomeController {
     private final UserService userService;
     private final NoteService noteService;
+    private static final String NOTE_SAVE_ERROR = "There was an error saving your note. Please try again.";
 
     @GetMapping()
     public String homeView(Authentication authentication, Model model) {
@@ -30,20 +31,22 @@ public class HomeController {
     @GetMapping(value = "/notes/delete/{noteId}")
     public String deleteNote(Authentication authentication, @PathVariable("noteId") int noteId, Model model) {
         Integer userId = getUserId(authentication);
-        String saveError = null;
+
         String error = null;
         try {
             int rowsAffected = noteService.deleteNote(userId, noteId);
             if (rowsAffected < 1) {
-                saveError = "There was an error saving your note. Please try again.";
+                error = NOTE_SAVE_ERROR;
             }
         } catch (Exception e) {
             error = e.getMessage();
         }
-        if (saveError == null && error == null) {
+
+        if (error == null) {
             model.addAttribute("saveSuccess", true);
+        } else if (error.equals(NOTE_SAVE_ERROR)) {
+            model.addAttribute("saveError", error);
         } else {
-            model.addAttribute("saveError", saveError);
             model.addAttribute("error", error);
         }
 
@@ -55,23 +58,23 @@ public class HomeController {
         Integer userId = getUserId(authentication);
         Integer noteId = request.getNoteId();
 
-        String saveError = null;
         String error = null;
 
         try {
             Note note = new Note(noteId, request.getNoteTitle(), request.getNoteDescription(), userId);
             int rowsAffected = noteId == null ? noteService.createNote(note) : noteService.updateNote(note);
             if (rowsAffected < 1) {
-                saveError = "There was an error saving your note. Please try again.";
+                error = NOTE_SAVE_ERROR;
             }
         } catch (Exception e) {
             error = e.getMessage();
         }
 
-        if (saveError == null && error == null) {
+        if (error == null) {
             model.addAttribute("saveSuccess", true);
+        } else if (error.equals(NOTE_SAVE_ERROR)) {
+            model.addAttribute("saveError", error);
         } else {
-            model.addAttribute("saveError", saveError);
             model.addAttribute("error", error);
         }
 
